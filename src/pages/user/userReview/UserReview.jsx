53c519b6041cf323665a647ee8224523d692f386
DevIsmail-coder <ismailolawale777@gmail.com>
2025-04-17 19:16:49 +0100

    updated the header component

---
+++ src/pages/user/userReview/UserReview.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const userReview = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default userReview
---
+++ src/pages/user/useraccount/Useraccount.jsx
@@ -0,0 +1,50 @@
+import React from 'react'
+import './useraccount.css'
+import { IoIosCall } from "react-icons/io";
+import { FaLocationDot } from "react-icons/fa6";
+import { FaLock } from "react-icons/fa";
+import { LiaUserEditSolid } from "react-icons/lia";
+
+const Useraccount = () => {
+  return (
+    <div className='AccountBody'>
+      <div className='Accountmain'>
+        <h3 className='Settings'>Account Settings</h3>
+        <p className='SettingsProfile'>
+        <LiaUserEditSolid  className='SettingsProfileIcon'/>
+        </p>
+        <span className='Securityspan'>
+        <input type="text" placeholder='Name' className='Securityspaninput'/>
+        </span> 
+
+         <span className='Securityspan'>
+        <input type="text" placeholder='Email' className='Securityspaninput'/>
+        </span> 
+
+       <span className='Securityspan'><IoIosCall  className='SettingIcon'/>
+        <input type="text" placeholder='Mobile Number' className='Securityspaninput'/>
+        </span>
+      
+       <span className='Securityspan2'><FaLocationDot className='SettingIcon' />
+        <input type="text" placeholder='Resedentail Address' className='Securityspaninput2'/>
+        </span>
+
+        <span className='Securityspan3'><FaLock  className='SettingIcon2'/>
+        <input type="text" placeholder='Enter Current Password' className='Securityspaninput3'/>
+        </span>
+         
+        <span className='Securityspan3'><FaLock className='SettingIcon2'/>
+        <input type="text" placeholder='Enter New Password' className='Securityspaninput3'/>
+        </span>
+
+        <span className='Securityspan3'><FaLock className='SettingIcon2'/>
+        <input type="text" placeholder='Comfirm New Password' className='Securityspaninput3'/>
+        </span>
+        
+        <button className='SettingButton'>update</button>
+      </div>
+    </div>
+  )
+}
+
+export default Useraccount
