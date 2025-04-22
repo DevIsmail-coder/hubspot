23176cbd361e22b1c1f57f1183057a85f424237c
DevIsmail-coder <ismailolawale777@gmail.com>
2025-04-22 08:58:05 +0100

    allmost done

--- src/pages/allhostdashboard/bookspacedetail/Bookspacedetail.jsx
+++ src/pages/allhostdashboard/bookspacedetail/Bookspacedetail.jsx
@@ -16,9 +16,9 @@ const Bookspace = () => {
   const handleResponse = (mess) => {
     console.log(mess?.res);
     if(mess.res?.data?.data){
-      setBookingList(mess.res?.data?.data)
+      setBookingList(mess.res?.data?.data?.Bookings)
 
-      console.log("am booking detail", mess?.res?.data?.data);
+      console.log("am booking detail", mess?.res?.data?.data?.Bookings);
     }
    
   }
@@ -35,14 +35,14 @@ const Bookspace = () => {
       <article className='Bookspacemain'>
         <header className='Bookspacemainhead'>
           <span className='Bookspacemainheadspan'>Renter</span>
-          <span className='Bookspacemainheadspan1'>Space</span>
+          {/* <span className='Bookspacemainheadspan1'>Space</span> */}
           <span className='Bookspacemainheadspan1'>Booking Dates</span>
           <span className='Bookspacemainheadspan1'>Booking Status</span>
         </header>
         <main className='Bookspacemainmain'>
           {
-            bookingList.map((i, id) => (
-              <article className='Bookspacemainmainwrap' key={id}>
+            bookingList.map((i, index) => (
+              <article className='Bookspacemainmainwrap' key={index}>
                 <div className='Bookspacemainmaindiv'>
                   <span className='Bookspacemainmaindivspan1'>
                     <HiUserCircle className='Bookspacemainmaindivspan1img' />
@@ -50,20 +50,19 @@ const Bookspace = () => {
                  
                       <p className='Bookspacemainmaindivspan2' >{i.userName}</p>
                 </div>
-                <div className='Bookspacemainmaindiv1'>{title}</div>
+                {/* <div className='Bookspacemainmaindiv1'>{title}</div> */}
                 
                     <div className='Bookspacemainmaindiv1' >
                       <span className='Bookspacemainmaindiv1span'>
-                        <p>{`${i.startDate?.slice(0,5)}${months[Number(i.startDate?.slice(5,7))-1]}${i.startDate?.slice(7,10)}`}</p>
-                        <p className='Bookspacemainmaindiv1spanp'></p>
+                        <p className='Bookspacemainmaindiv1spanp'>{`${i.startDate?.slice(0,5)}${months[Number(i.startDate?.slice(5,7))-1]}${i.startDate?.slice(7,10)}`}</p>
+                    
                       </span>  -
                       <span className='Bookspacemainmaindiv1span'>
-                        {/* <p>{`${i.endDate?.slice(0,5)}${months[Number(i.endDate?.slice(5,7))-1]}${i.endDate?.slice(7,10)}`}</p> */}
-                        <p className='Bookspacemainmaindiv1spanp'>{i.endTime}</p>
+                        <p className='Bookspacemainmaindiv1spanp'>{`${i.endDate?.slice(0,5)}${months[Number(i.endDate?.slice(5,7))-1]}${i.endDate?.slice(7,10)}`}</p>
                       </span>
                     </div>
 
-                <div className='Bookspacemainmaindiv1'>
+                <div className='Bookspacemainmaindiv3'>
                   <span className='Bookspacemainmaindiv1spanXX'
                     style={{
                       backgroundColor: i.status === 'upcoming' ? '#FEFAEE' : i.status === 'active' ? '#E9F7EF' : '#FFEAEB',
--- src/pages/user/userReview/Usereview.jsx
+++ src/pages/user/userReview/Usereview.jsx
@@ -1,33 +1,68 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './usereview.css'
+import { useSelector } from 'react-redux';
+import { userBooking } from '../../Hubspotapi';
 
 const Usereview = () => {
-  return (
-    <div className='userReviewBookingpage'>
-    <table className='userReviewFlowData'>
-        <thead className='userReviewFlowdata1'>
-            <tr>
-                <th className='userReviewDataInfo'>Booking ID</th>
-                <th className='userReviewDataInfo'>Space Name</th>
-                <th className='userReviewDataInfo'>Dates</th>
-                <th className='userReviewDataInfo'>Status</th>
-                <th className='userReviewDataInfo'>Action</th>
-            </tr>
-        </thead>
-        <tbody className='userReviewDatainfoB'>
-            <tr className='userReviewDatainfoAB'>
-                <td className='userReviewDataInfo2'>FS-4821</td>
-                <td className='userReviewDataInfo2'><div className='userReviewSpaceDeet'> <img className='userReviewChoiceImage' src="yourimage.jpg" alt="" width="30" /> <div className='userReviewspaceDeets'><p>Flexi Space</p><p>Co work</p></div></div></td>
-                <td className='userReviewDataInfo2'>2 Apr - 5 Apr</td>
-                <td className='userReviewDataInfo2'>Upcoming</td>
-                <td className='userReviewDataInfo2' ><button className='userReviewReviewbutton'>Add Review</button></td>
-            </tr>
-        </tbody>
-    </table>
+    const hostShowToken = useSelector((state) => state.hubspot.userToken);
+    const bookToken = hostShowToken.userToken
+    const [booking, setBooking] = useState([])
 
-</div>
 
-  )
+    const handleResponse = (mess) => {
+        if(mess?.res?.data?.data){
+            setBooking(mess?.res?.data?.data);
+            console.log(booking);
+            
+        }
+    }
+
+    useEffect(() => {
+        userBooking(handleResponse, bookToken)
+    }, [])
+    return (
+        <div className='userReviewBookingpage'>
+            <table className='userReviewFlowData'>
+                <thead className='userReviewFlowdata1'>
+                    {/* <tr> */}
+                        {/* <th className='userReviewDataInfo'>Booking ID</th> */}
+                        <th className='userReviewDataInfo'>Space Name</th>
+                        <th className='userReviewDataInfo'>Dates</th>
+                        <th className='userReviewDataInfo'>Status</th>
+                        <th className='userReviewDataInfo'>Action</th>
+                    {/* </tr> */}
+                </thead>
+                <tbody className='userReviewDatainfoB'>
+                 {
+                    booking.map((i, id) => (
+                        <div key={id} className='userReviewFlowdatab2'>
+                        {/* <td className='userReviewDataInfo2'>FS-4821</td> */}
+                        <td className='userReviewDataInfo2'>{i.spaceName}</td>
+                        <td className='userReviewDataInfo2'>2 Apr - 5 Apr</td>
+                        <td className='userReviewDataInfo2'> 
+                            <span 
+                            className='userReviewDataInfo2span'
+                              style={{
+                                backgroundColor: i.status === 'upcoming' ? '#FEFAEE' : i.status === 'active' ? '#E9F7EF' : '#FFEAEB',
+                                color: i.status === 'upcoming' ? '#F5D77A' : i.status === 'active' ? '#27AE60' : '#EB5857'
+                              }}
+                            >
+    
+                            {i.status}
+                            </span>
+                            
+                            </td>
+                        <td className='userReviewDataInfo2' ><button className='userReviewReviewbutton'>Add Review</button></td>
+                    </div>
+                    ))
+               
+                 }
+                </tbody>
+            </table>
+
+        </div>
+
+    )
 }
 
 export default Usereview
